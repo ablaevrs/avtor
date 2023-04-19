ba4a58e240883fea2bce3a57e7b693266bbb8cd3
rustem <ablaev.r.s@gmail.com>
2023-04-19 21:55:18 +0300

    server: connect db

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+
+dotenv.config()
+connectDB()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('First example')
+})
+
+app.listen(PORT, () => console.log('Server started'))
