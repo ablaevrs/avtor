8adf2f2e4ee272f5d6f956e120d2095bb604db38
rustem <ablaev.r.s@gmail.com>
2023-04-21 19:17:56 +0300

    server: add login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -31,3 +31,30 @@ export const register = async (req, res) => {
         res.status(500).json({ message: 'Error:' + error })
     }
 }
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    if (
+        !email && email.trim() === '' &&
+        !password && password.length < 3
+    ) {
+        return res.status(422).json({ message: 'Invalid data' })
+    }
+
+    let existingUser
+    try {
+        existingUser = await User.findOne({ email, password })
+
+        if (!existingUser) {
+            res.status(422).json({ message: 'Wrong email or password'})
+        } 
+
+        return res.status(200).json({ id: existingUser._id, message: 'Login successfull' })
+
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error:' + error })
+    }
+
+}
