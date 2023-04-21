63a0ada5d3a7ee53704d02a1929a5e4037635d30
rustem <ablaev.r.s@gmail.com>
2023-04-21 19:46:08 +0300

    server: add bcrypt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,45 +1,68 @@
 import User from '../model/User.js'
+import bcrypt from 'bcrypt'
 
 export const register = async (req, res) => {
     try {
         const { name, email, password } = req.body
 
+        // simple validation
         if (
             !name && name.trim() === '' &&
             !email && email.trim() === '' &&
             !password && password.length < 3
         ) {
-            return res.status(422).json({ message: 'Invalid data' })
+            return res.status(400).send({ 
+                success: false,
+                message: 'Please fill all fields' 
+            })
         }
 
         // check whether the user exists or not
         let user = await User.findOne({ email: email })
 
         if(user) {
-            return res.status(400).json({ message: 'User have already register'})
+            return res.status(401).send({ 
+                success: false,
+                message: 'User already exists'
+            })
         } 
 
+        const hashPassword = bcrypt.hash(password, 10)
+
+        // save user
         user = await User.create({
             name,
             email,
-            password
+            password: hashPassword
         })
 
-        return res.status(201).json({ message: 'User created'})
+        return res.status(201).send({
+            success: true, 
+            message: 'User created',
+            user
+        })
         
     } catch (error) {
-        res.status(500).json({ message: 'Error:' + error })
+        return res.status(500).send({ 
+            message: 'Error in Register callback',
+            success: false,
+            error
+        })
     }
 }
 
 export const login = async (req, res) => {
     const { email, password } = req.body
 
+    // simple validation
     if (
         !email && email.trim() === '' &&
         !password && password.length < 3
     ) {
-        return res.status(422).json({ message: 'Invalid data' })
+        return res.status(401).send({ 
+            success: false,
+            message: 'Fill fields correct'
+        })
     }
 
     let existingUser
@@ -47,14 +70,33 @@ export const login = async (req, res) => {
         existingUser = await User.findOne({ email, password })
 
         if (!existingUser) {
-            res.status(422).json({ message: 'Wrong email or password'})
+            return res.status(401).send({ 
+                success: false,
+                message: 'Wrong email or password'
+            })
         } 
 
-        return res.status(200).json({ id: existingUser._id, message: 'Login successfull' })
+        // check password
+        const isMatch = await bcrypt.compare(password, existingUser.password)
+        if (!isMatch) {
+            return res.status(401).send({ 
+                success: false,
+                message: 'Wrong email or password'
+            })
+        } 
 
+        return res.status(200).send({
+            success: true, 
+            message: 'Login successfull',
+            existingUser
+        })
 
     } catch (error) {
-        res.status(500).json({ message: 'Error:' + error })
+        return res.status(500).send({ 
+            message: 'Error in Login callback',
+            success: false,
+            error
+        })
     }
 
 }
