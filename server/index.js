5988ea30b914f19522f22b329ed0ea5edc39eb60
rustem <ablaev.r.s@gmail.com>
2023-04-22 22:11:56 +0300

    server: add multer

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import multer from 'multer'
 import connectDB from './config/db.js'
 
 // Routes
@@ -12,8 +13,27 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+// create storage for files
+const storage = multer.diskStorage({
+    destination: (_, __, cd) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage })
+
 app.use(cors())
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
+
+// create route for multer
+app.post('/upload', upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
 
 app.use('/auth', authRoutes)
 app.use('/post', postRoutes)
